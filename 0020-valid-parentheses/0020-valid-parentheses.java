c041062e442c7ead5f49f387150f891287f7e15b
Harsh Raj <harshcodes.sshhr@gmail.com>
2026-05-31 21:03:43 +0530

    Time: 3 ms (88.1%), Space: 43.4 MB (40.16%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,42 +1,30 @@
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> stack = new Stack();
-        for(char i : s.toCharArray()){
-            if(isBracketOpen(i)){
-                stack.push(i);
+        Stack <Character> stack = new Stack<>();
+
+        for(int i=0; i<s.length(); i++){
+            char ch = s.charAt(i);
+
+            if(ch == '(' || ch == '{' || ch == '['){
+                stack.push(ch);
             }else{
-                if (stack.size() == 0){
-                    return false;
-                }else{
-                    if (areBracketsMatching(stack.peek(),i)){
-                        stack.pop();
-                    }else{
-                        return false;
-                    }
+                if(stack.isEmpty()){
+                return false;
                 }
+            
+            
+            if(ch == ')' && stack.peek() == '('){
+                stack.pop();
+            }else if(ch == '}' && stack.peek() == '{'){
+                stack.pop();
+            }else if(ch == ']' && stack.peek() == '['){
+                stack.pop();
+            }else{
+                return false;
+            }
             }
         }
-        return stack.size()== 0;
-    }
-    private boolean isBracketOpen(char c){
-        switch(c){
-            case '(':
-            case '{':
-            case '[':
-                return true;
-            default: 
-                return false;
-        }
-    }
-    private boolean areBracketsMatching(char c, char d){
-        if(c == '(' && d== ')'){
-            return true;
-        }else if(c == '{' && d == '}'){
-            return true;
-        }else if(c == '[' && d == ']'){
-            return true;
-        }else{
-            return false;
-        }
+        return stack.isEmpty();
+        
     }
 }
