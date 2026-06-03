f1af46864763e77453340680b6739379f98507a8
Harsh Raj <harshcodes.sshhr@gmail.com>
2026-06-03 20:16:01 +0530

    Time: 0 ms (100%), Space: 48 MB (85.09%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,36 +1,58 @@
 class Solution {
+
     public int[] searchRange(int[] nums, int target) {
-        int[] result = {-1, -1};
-        int left = binarySearch(nums, target, true);
-        int right = binarySearch(nums, target, false);
-        result[0] = left;
-        result[1] = right;
-        return result;        
+
+        int[] res = {-1, -1};
+
+        res[0] = FirstOccurrence(nums, target);
+        res[1] = LastOccurrence(nums, target);
+
+        return res;
     }
 
-    private int binarySearch(int[] nums, int target, boolean isSearchingLeft) {
-        int left = 0;
-        int right = nums.length - 1;
-        int idx = -1;
+    public int FirstOccurrence(int[] nums, int target) {
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            
-            if (nums[mid] > target) {
-                right = mid - 1;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
+        int start = 0;
+        int end = nums.length - 1;
+        int ans = -1;
+
+        while(start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            if(nums[mid] < target) {
+                start = mid + 1;
+            } else if(nums[mid] > target) {
+                end = mid - 1;
             } else {
-                idx = mid;
-                if (isSearchingLeft) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
+                ans = mid;    
+                end = mid - 1; 
             }
         }
 
-        return idx;
+        return ans;
     }
 
+    public int LastOccurrence(int[] nums, int target) {
+
+        int start = 0;
+        int end = nums.length - 1;
+        int ans = -1;
+
+        while(start <= end) {
+
+            int mid = start + (end - start) / 2;
+
+            if(nums[mid] < target) {
+                start = mid + 1;
+            } else if(nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                ans = mid;
+                start = mid + 1; 
+            }
+        }
+
+        return ans;
+    }
 }
