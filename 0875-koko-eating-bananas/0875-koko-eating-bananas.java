dbc394ac1e7816e787ca774fb9f5ecda5cfc6eda
Harsh Raj <harshcodes.sshhr@gmail.com>
2026-06-04 16:34:24 +0530

    Time: 12 ms (54.69%), Space: 48.6 MB (9.03%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,38 +1,33 @@
 class Solution {
-    
-    public boolean canEatAll(int[] piles, int mid, int h) {
-        int actualHour = 0;
-        
-        for (int i : piles) {
-            actualHour += i / mid;
-            
-            if (i % mid != 0) {
-                actualHour++;
-            }
-        }
-        
-        return actualHour <= h;
-    }
-
     public int minEatingSpeed(int[] piles, int h) {
-        
-        int l = 1;
-        int r = 0;
-
-        for (int i : piles) {
-            r = Math.max(r, i);
+        int start = 1;
+        int end = 0;
+        for(int i = 0; i<piles.length;i++){
+            end = Math.max(end,piles[i]);
         }
+        int res = -1;
 
-        while (l < r) {
-            int mid = l + (r - l) / 2;
-
-            if (canEatAll(piles, mid, h)) {
-                r = mid;
-            } else {
-                l = mid + 1;
+        while(start <= end){
+            int mid = start +(end - start)/2;
+            long hour = hours(piles,h,mid);
+            if(hour > h){
+                start = mid + 1;
+            }else{
+                res = mid;
+                end = mid - 1;
             }
         }
-
-        return l;
+        return res;
     }
+    public long hours(int[] piles, int h, int speed){
+        long c =0;
+        for(int i=0;i<piles.length;i++){
+            c = c + piles[i] / speed;
+            if(piles[i] % speed != 0){
+                c++;
+            }
+        }
+        return c;
+    }
+
 }
