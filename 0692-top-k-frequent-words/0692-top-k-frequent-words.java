b6b3311b0a718c47df667bb1f000fdb28b54acfd
Harsh Raj <harshcodes.sshhr@gmail.com>
2026-06-06 14:27:18 +0530

    Time: 7 ms (84.8%), Space: 46.7 MB (16.53%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -0,0 +1,53 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+
+        int n = words.length;
+
+        PriorityQueue<Pair<Integer, String>> pq = new PriorityQueue<>(
+            (a, b) -> {
+                if (!a.getKey().equals(b.getKey())) {
+                    return a.getKey() - b.getKey(); // Min frequency
+                }
+                return b.getValue().compareTo(a.getValue()); // Max lexicographical
+            }
+        );
+
+        HashMap<String, Integer> freq = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            freq.put(words[i], freq.getOrDefault(words[i], 0) + 1);
+        }
+
+        for (Map.Entry<String, Integer> entry : freq.entrySet()) {
+
+            String word = entry.getKey();
+            int frequency = entry.getValue();
+
+            Pair<Integer, String> curr =
+                new Pair<>(frequency, word);
+
+            if (pq.size() < k) {
+                pq.add(curr);
+                continue;
+            }
+
+            Pair<Integer, String> top = pq.peek();
+
+            if (curr.getKey() > top.getKey()
+                || (curr.getKey().equals(top.getKey())
+                    && curr.getValue().compareTo(top.getValue()) < 0)) {
+
+                pq.poll();
+                pq.add(curr);
+            }
+        }
+
+        LinkedList<String> res = new LinkedList<>();
+
+        while (!pq.isEmpty()) {
+            res.addFirst(pq.poll().getValue());
+        }
+
+        return res;
+    }
+}
