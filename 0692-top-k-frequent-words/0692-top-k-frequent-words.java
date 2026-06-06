e1c89b1744079eb4bb346468ea1cf1453c7043fa
Harsh Raj <harshcodes.sshhr@gmail.com>
2026-06-06 14:31:18 +0530

    Time: 8 ms (54.31%), Space: 45.7 MB (99.69%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,42 +1,52 @@
+import java.util.*;
+
+class Pair {
+    int first;
+    String second;
+
+    Pair(int f, String s) {
+        first = f;
+        second = s;
+    }
+}
+
 class Solution {
+
     public List<String> topKFrequent(String[] words, int k) {
 
         int n = words.length;
 
-        PriorityQueue<Pair<Integer, String>> pq = new PriorityQueue<>(
+        PriorityQueue<Pair> pq = new PriorityQueue<>(
             (a, b) -> {
-                if (!a.getKey().equals(b.getKey())) {
-                    return a.getKey() - b.getKey(); // Min frequency
-                }
-                return b.getValue().compareTo(a.getValue()); // Max lexicographical
+                if(a.first != b.first)
+                    return a.first - b.first;      // Min frequency
+
+                return b.second.compareTo(a.second); // Max word
             }
         );
 
         HashMap<String, Integer> freq = new HashMap<>();
 
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++) {
             freq.put(words[i], freq.getOrDefault(words[i], 0) + 1);
         }
 
-        for (Map.Entry<String, Integer> entry : freq.entrySet()) {
+        for(Map.Entry<String, Integer> entry : freq.entrySet()) {
 
             String word = entry.getKey();
             int frequency = entry.getValue();
 
-            Pair<Integer, String> curr =
-                new Pair<>(frequency, word);
+            Pair curr = new Pair(frequency, word);
 
-            if (pq.size() < k) {
+            if(pq.size() < k) {
                 pq.add(curr);
                 continue;
             }
 
-            Pair<Integer, String> top = pq.peek();
-
-            if (curr.getKey() > top.getKey()
-                || (curr.getKey().equals(top.getKey())
-                    && curr.getValue().compareTo(top.getValue()) < 0)) {
-
+            if(curr.first > pq.peek().first ||
+               (curr.first == pq.peek().first &&
+                curr.second.compareTo(pq.peek().second) < 0))
+            {
                 pq.poll();
                 pq.add(curr);
             }
@@ -44,8 +54,8 @@ class Solution {
 
         LinkedList<String> res = new LinkedList<>();
 
-        while (!pq.isEmpty()) {
-            res.addFirst(pq.poll().getValue());
+        while(!pq.isEmpty()) {
+            res.addFirst(pq.poll().second);
         }
 
         return res;
