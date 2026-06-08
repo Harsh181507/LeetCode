6ce7f8f77978da8bf6688ad3eac1331f1b1872b4
Harsh Raj <harshcodes.sshhr@gmail.com>
2026-06-08 23:54:10 +0530

    Time: 0 ms (100%), Space: 43.5 MB (52.02%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,12 +1,6 @@
 class Solution {
     public List<String> letterCombinations(String digits) {
-        List<String> res = new ArrayList<>();
-        
-        if (digits == null || digits.length() == 0) {
-            return res;
-        }
-        
-        Map<Character, String> digitToLetters = new HashMap<>();
+        HashMap<Character,String> digitToLetters = new HashMap<>();
         digitToLetters.put('2', "abc");
         digitToLetters.put('3', "def");
         digitToLetters.put('4', "ghi");
@@ -15,24 +9,27 @@ class Solution {
         digitToLetters.put('7', "pqrs");
         digitToLetters.put('8', "tuv");
         digitToLetters.put('9', "wxyz");
+        int n = digits.length();
         
-        backtrack(digits, 0, new StringBuilder(), res, digitToLetters);
-        
-        return res;        
+        ArrayList<String> res = new ArrayList<>();
+        fun(digits,n,0,new StringBuilder(),res,digitToLetters);
+        return res;
+
     }
 
-    private void backtrack(String digits, int idx, StringBuilder comb, 
-    List<String> res, Map<Character, String> digitToLetters) {
-        if (idx == digits.length()) {
-            res.add(comb.toString());
+    public void fun(String digits, int n, int idx, StringBuilder diary,ArrayList <String> res,HashMap<Character,String> digitToLetters){
+        if(idx == n){
+            res.add(diary.toString());
             return;
         }
-        
-        String letters = digitToLetters.get(digits.charAt(idx));
-        for (char letter : letters.toCharArray()) {
-            comb.append(letter);
-            backtrack(digits, idx + 1, comb, res, digitToLetters);
-            comb.deleteCharAt(comb.length() - 1);
+
+        char digit = digits.charAt(idx);
+        String choice = digitToLetters.get(digit);
+        for(int j =0;j<choice.length();j++){
+            diary.append(choice.charAt(j));
+            fun(digits,n,idx+1,diary,res,digitToLetters);
+            diary.deleteCharAt(diary.length()-1);
         }
-    }    
+        return;
+    }
 }
