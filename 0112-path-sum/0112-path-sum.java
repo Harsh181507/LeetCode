f19a3fe6049eb628752dbb37225ae9368b3ca4a3
Harsh Raj <harshcodes.sshhr@gmail.com>
2026-06-11 17:01:37 +0530

    Time: 0 ms (100%), Space: 45.2 MB (13.98%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -1,16 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    public boolean rootToLeafPathSum(TreeNode root, int targetSum, int sum){
-        if(root == null)
-            return false;
-        if(root.left == null && root.right == null){
-            sum = sum + root.val;
-            if(sum == targetSum)
-                return true;   
-        }
-        return rootToLeafPathSum(root.left, targetSum, sum + root.val) || rootToLeafPathSum(root.right, targetSum, sum + root.val);
-    }
+
+    boolean res = false;
+
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        int sum = 0;
-        return rootToLeafPathSum(root, targetSum, sum);
+        fun(root, 0, targetSum);
+        return res;
+    }
+    public void fun(TreeNode root, int sum, int targetSum){
+        if(root == null){
+            return;
+        }
+        sum += root.val;
+        if(root.left == null && root.right == null){
+            if(sum == targetSum){
+                res = true;
+            }
+            return;
+        }
+        fun(root.left, sum, targetSum);
+        fun(root.right, sum, targetSum);
     }
 }
