f8006b6d16cfeb1cc25685e8230b2b8fc3a12783
Harsh Raj <harshcodes.sshhr@gmail.com>
2026-06-13 01:38:54 +0530

    Time: 4 ms (44.42%), Space: 52.3 MB (59.31%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,29 +1,37 @@
 class Solution {
+    int[] x = {-1, 1, 0, 0};
+    int[] y = {0, 0, -1, 1};
     public int numIslands(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int c = 0;
-        
-        for(int i =0;i<m;i++){
-            for(int j =0; j <n ;j++){
-                if(grid[i][j] == '1'){
-                    dfs(grid,i,j);
-                    c++;
+        int n = grid.length;
+        int m = grid[0].length;
+        int res = 0;
+        boolean[][] vis = new boolean[n][m];
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j] == '1' && vis[i][j] == false){
+                    dfs(grid, n, m, i, j, vis);
+                    res++;
                 }
             }
         }
-        return c;
+        return res;
     }
-    public void dfs(char[][] grid, int i, int j){
-        int m = grid.length;
-        int n = grid[0].length;
-        if(i < 0 || j < 0 || i>=m || j>=n || grid[i][j] == '0'){
-            return;
+    public boolean valid(int i, int j, int n, int m){
+        if(i < 0 || i >= n || j < 0 || j >= m){
+            return false;
         }
-        grid[i][j] ='0';
-        dfs(grid,i+1,j);
-        dfs(grid,i-1,j);
-        dfs(grid,i,j+1);
-        dfs(grid,i,j-1);
+        return true;
+    }
+    public void dfs(char[][] grid,int n,int m,int i,int j,boolean[][] vis){
+        vis[i][j] = true;
+        for(int k = 0; k < 4; k++){
+            int row = i + x[k];
+            int col = j + y[k];
+            if(valid(row, col, n, m)
+                && grid[row][col] == '1'
+                && vis[row][col] == false){
+                dfs(grid, n, m, row, col, vis);
+            }
+        }
     }
 }
