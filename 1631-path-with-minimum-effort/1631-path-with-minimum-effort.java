987e51540ddca70ef2b488634258d6b38932f23a
Harsh Raj <harshcodes.sshhr@gmail.com>
2026-06-15 12:13:09 +0530

    Time: 53 ms (37.08%), Space: 48 MB (39.74%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.java
@@ -0,0 +1,65 @@
+class Pair{
+    int first;
+    int second;
+    int third;
+    Pair(int f,int s,int t){
+        first = f;
+        second = s;
+        third = t;
+    }
+}
+class Solution {
+    int[] x = {1,-1,0,0};
+    int[] y = {0,0,1,-1};
+    public int minimumEffortPath(int[][] heights) {
+        int n = heights.length;
+        int m = heights[0].length;
+        int[][] res = new int[n][m];
+        for(int i = 0; i < n; i++){
+            Arrays.fill(res[i], Integer.MAX_VALUE);
+        }
+        PriorityQueue<Pair> pq =
+            new PriorityQueue<>(
+                (a,b) -> a.first - b.first
+            );
+        res[0][0] = 0;
+        pq.add(new Pair(0,0,0));
+        while(!pq.isEmpty()){
+            Pair p = pq.poll();
+            int dist = p.first;
+            int row = p.second;
+            int col = p.third;
+            if(dist > res[row][col]){
+                continue;
+            }
+            for(int k = 0; k < 4; k++){
+                int r = row + x[k];
+                int c = col + y[k];
+                if(!valid(r,c,n,m)){
+                    continue;
+                }
+                int absdiff =Math.abs(heights[row][col]-heights[r][c]);
+                int newwt =Math.max(dist,absdiff);
+                if(newwt < res[r][c]){
+                    res[r][c] = newwt;
+                    pq.add(new Pair(newwt,r, c));                        
+                }
+            }
+        }
+        return res[n-1][m-1];
+    }
+
+    public boolean valid(
+        int i,
+        int j,
+        int n,
+        int m
+    ){
+
+        if(i < 0 || i >= n || j < 0 || j >= m){
+            return false;
+        }
+
+        return true;
+    }
+}
