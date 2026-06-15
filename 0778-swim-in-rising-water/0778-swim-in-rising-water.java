e639f57413306554ac17d1bb5f60c215dd4586b4
Harsh Raj <harshcodes.sshhr@gmail.com>
2026-06-15 21:14:06 +0530

    Time: 13 ms (38.71%), Space: 46.4 MB (61.07%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.java
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.java
@@ -1,96 +1,50 @@
 class Pair{
-    int first;
-    int second;
-    int third;
-
+    int first,second,third;
     Pair(int f,int s,int t){
-        first = f;
-        second = s;
-        third = t;
+        first=f;
+        second=s;
+        third=t;
     }
 }
-
 class Solution {
-
-    int[] x = {1,-1,0,0};
-    int[] y = {0,0,1,-1};
-
+    int[] x={1,-1,0,0};
+    int[] y={0,0,1,-1};
     public int swimInWater(int[][] grid) {
-
-        int n = grid.length;
-
-        int[][] res = new int[n][n];
-
-        for(int i = 0; i < n; i++){
-            Arrays.fill(res[i], Integer.MAX_VALUE);
+        int n=grid.length;
+        int[][] res=new int[n][n];
+        for(int i=0;i<n;i++){
+            Arrays.fill(res[i],Integer.MAX_VALUE);
         }
-
-        PriorityQueue<Pair> pq =
-            new PriorityQueue<>(
-                (a,b) -> a.first - b.first
-            );
-
-        res[0][0] = grid[0][0];
-
-        pq.add(
-            new Pair(
-                grid[0][0],
-                0,
-                0
-            )
-        );
-
+        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->a.first-b.first);
+        res[0][0]=grid[0][0];
+        pq.add(new Pair(grid[0][0],0,0));
         while(!pq.isEmpty()){
-
-            Pair p = pq.poll();
-
-            int dist = p.first;
-            int row = p.second;
-            int col = p.third;
-
-            if(dist > res[row][col]){
+            Pair p=pq.poll();
+            int dist=p.first;
+            int row=p.second;
+            int col=p.third;
+            if(dist>res[row][col]){
                 continue;
             }
-
-            for(int k = 0; k < 4; k++){
-
-                int r = row + x[k];
-                int c = col + y[k];
-
+            for(int k=0;k<4;k++){
+                int r=row+x[k];
+                int c=col+y[k];
                 if(!valid(r,c,n,n)){
                     continue;
                 }
-
-                int newwt =
-                    Math.max(
-                        dist,
-                        grid[r][c]
-                    );
-
-                if(newwt < res[r][c]){
-
-                    res[r][c] = newwt;
-
-                    pq.add(
-                        new Pair(
-                            newwt,
-                            r,
-                            c
-                        )
-                    );
+                int newwt=Math.max(dist,grid[r][c]);
+                if(newwt<res[r][c]){
+                    res[r][c]=newwt;
+                    pq.add(new Pair(newwt,r,c));
                 }
             }
         }
-
         return res[n-1][n-1];
     }
-
     public boolean valid(int i,int j,int n,int m){
-
-        if(i < 0 || i >= n || j < 0 || j >= m){
+        if(i<0 || i>=n || j<0 || j>=m){
             return false;
         }
-
         return true;
     }
 }
