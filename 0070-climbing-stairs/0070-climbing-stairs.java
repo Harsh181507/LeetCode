b8bcbd97967c7cb2e5290e79fc9cf3f9bdc4e830
Harsh Raj <harshcodes.sshhr@gmail.com>
2026-06-16 21:34:23 +0530

    Time: 0 ms (100%), Space: 42.2 MB (34.86%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,15 +1,14 @@
 class Solution {
-    HashMap<Integer,Integer>dp = new HashMap<>();
     public int climbStairs(int n) {
-        if(n==1 || n == 0){
+        int [] dp = new int [n+1];
+        if(n==0 || n==1){
             return 1;
+        }    
+        dp[0] =1;
+        dp[1] =1;
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1] + dp[i-2];
         }
-        if(dp.containsKey(n)){
-            return dp.get(n);
-        }
-        int ans = climbStairs(n-1) + climbStairs(n-2);
-        dp.put(n,ans);
-        return ans;
-        
+        return dp[n];
     }
 }
