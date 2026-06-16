0e54435b80a36e2ec4634011be6985850868f554
Harsh Raj <harshcodes.sshhr@gmail.com>
2026-06-16 12:06:32 +0530

    Time: 0 ms (100%), Space: 42.3 MB (15.89%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,9 +1,14 @@
 class Solution {
+    HashMap<Integer,Integer> dp = new HashMap<>();
     public int fib(int n) {
-        if(n == 1 || n == 0){
+        if(n == 0 || n == 1){
             return n;
         }
-        int ans = fib(n-1) + fib(n-2);
+        if(dp.containsKey(n)){
+            return dp.get(n);
+        }
+        int ans = fib(n - 1) + fib(n - 2);
+        dp.put(n, ans);
         return ans;
     }
 }
