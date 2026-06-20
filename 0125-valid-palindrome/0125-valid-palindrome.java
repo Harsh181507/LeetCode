818609f6ce99f48f7e27f02ac27ac41e360cb94f
Harsh Raj <harshcodes.sshhr@gmail.com>
2026-06-20 12:57:01 +0530

    Time: 2 ms (99.21%), Space: 44.3 MB (65.57%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,7 +1,8 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int i =0;
-        int j = s.length()-1;
+        int i=0;
+        int j= s.length() - 1;
+
         while(i<j){
             while(i<j && !Character.isLetterOrDigit(s.charAt(i))){
                 i++;
