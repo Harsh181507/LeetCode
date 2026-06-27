f6d775b768dd7ceceb4174f59a516123f53abeea
Harsh Raj <harshcodes.sshhr@gmail.com>
2026-06-27 21:34:58 +0530

    Time: 0 ms (100%), Space: 43.6 MB (75.96%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m-1;
-        int j = n-1;
+        int i=m-1;
+        int j=n-1;
         int k = m+n-1;
         while(i>=0 && j>=0){
             if(nums1[i] < nums2[j]){
@@ -13,12 +13,11 @@ class Solution {
                 i--;
                 k--;
             }
-            
         }
         while(j>=0){
             nums1[k] = nums2[j];
-            j--;
             k--;
+            j--;
         }
     }
 }
